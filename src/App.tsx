4f4e9488df5984ac510d865042cc05d06132a575
leye20 <leyeagbo@gmail.com>
2020-04-08 18:29:50 -0600

    identity added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Heading } from './components/Heading';
+import Identity from './components/Identity';
 import logo from './logo.svg';
 import './App.css';
+
+const myIdentityFunction = ( input: string ): string => {
+  return `Wondering why we are number ONE! ${input}`; // template literal format
+  // return 'Wondering why we are number ONE! ' + input; normal or traditional way.
+}
 
 function App() {
   return (
@@ -20,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <Identity name="Smith Jones" age="26" role="Data Scientist" department="TechData" displayMethod={myIdentityFunction} />
       </header>
     </div>
   );
